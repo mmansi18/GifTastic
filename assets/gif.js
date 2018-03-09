d7b7145687b38cc29df87afcd030ae64b8e5d522
Michael Mansi <mmansi18@gmail.com>
2018-03-09 01:14:56 -0800

    added buttons on js

--- assets/gif.js
+++ assets/gif.js
@@ -1,7 +1,36 @@
-var movies = ["Pineapple Express", "Half Baked", "Seven Psychopaths", "40 Year-old Virgin", "Wall-e"];
+var movies = ["Pineapple Express", "Half Baked", "Seven Psychopaths", "The 40-Year-Old Virgin", "WALL-E"];
+
+//function for displaying movie data
 function renderButtons(){
-    $("giphy-view").html("")
-    for (i=0; i < movies.length; i++){
-        $("#giphy-view").append('<button class="btn btn-outline-light giphy-info" value="${movies[i]}">${movies[i]}</button>')
+
+    //deleting the movies prior to adding new movies, so we dont have repeat buttons
+    $("giphy-view").empty
+
+    //looping through array of movies
+    for (var i=0; i < movies.length; i++){
+
+        //generating buttons
+        var a = $("<button>");
+        //add class
+        a.addClass("movie-btn");
+        //adding data attribute
+        a.attr("data-name", movies[i]);
+        //button text
+        a.text(movies[i]);
+        //adding button to div
+        $("#giphy-view").append(a);
     }
 }
+   
+//event where a movie button is clicked
+$("#add-giphy").on("click", function(event) {
+    event.preventDefault();
+    //grabs the input from the textbox
+    var movie = $("#giphy-input").val().trim();
+    //adding movie from the textbox to our array
+    movies.push(movie);
+    //processing of our movie array
+    renderButtons();
+});
+
+renderButtons();
